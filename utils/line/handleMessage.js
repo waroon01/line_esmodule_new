dc8e85f190857bb4269296e751fd763a3deadb9f
Waroonporn Rattanabhutchai <waroon01@gmail.com>
2025-08-17 20:45:04 +0700

    line add

--- utils/line/handleMessage.js
+++ utils/line/handleMessage.js
@@ -12,7 +12,7 @@ export const handleMessage = async (event) => {
         });
       } else if (event.message.text.startsWith("doc/")) {
         const query = event.message.text.substring(4).trim(); // เอาข้อความหลัง "doc/"
-        console.log(query)
+        console.log("query ",query)
         // สมมติให้ใช้เป็น fullNumber
         const letters = await findDocuments({ fullNumber: query });
 
