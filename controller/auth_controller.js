1a2ff38b7779529c652d484b701f66b5cc63f0ac
Waroonporn Rattanabhutchai <waroon01@gmail.com>
2025-08-17 14:15:30 +0700

    prisma ok ja

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import prisma from "../config/prisma_config";
 
 export const register = async(req,res,next)=>{
     try{
