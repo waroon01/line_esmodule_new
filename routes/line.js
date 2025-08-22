d3a899846c50bec2ac5e40f8f4e4b7cd84209a6e
waroon01 <waroon01@gmail.com>
2025-08-22 16:15:16 +0700

    test

--- routes/line.js
+++ routes/line.js
@@ -7,7 +7,7 @@ import {
 } from "../config/line_config.js";
 import handleEvent from "../controller/line_controller.js";
 import path from "path";
-import fs from "fs";
+import { readFileSync } from "node:fs"; // ✅ ใช้ import แทน require
 
 const router = express.Router();
 
@@ -21,8 +21,68 @@ router.post("/webhook", line.middleware(lineConfig), (req, res) => {
 });
 
 router.post("/createrichmenu", async (req, res) => {
-  console.log("test")
-  res.json({message: "success"})
+  try {
+    const richMenu = {
+  size: {
+    width: 2500,
+    height: 1686
+  },
+  selected: false,
+  name: "richmenu-a",
+  chatBarText: "Tap to open",
+  areas: [
+    {
+      bounds: {
+        x: 0,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "uri",
+        uri: "https://developers.line.biz/"
+      }
+    },
+    {
+      bounds: {
+        x: 1251,
+        y: 0,
+        width: 1250,
+        height: 1686
+      },
+      action: {
+        type: "richmenuswitch",
+        richMenuAliasId: "richmenu-alias-b",
+        data: "richmenu-changed-to-b"
+      }
+    }
+  ]
+};
+
+    // สร้าง rich menu
+    const { richMenuId } = await lineclient.createRichMenu(richMenu);
+    console.log("✅ Created richmenu:", richMenuId);
+
+    // อัพโหลดรูป
+    const imagePath = path.resolve("./public/richmenu-a.png");
+    const bufferA = readFileSync(imagePath)
+
+    await lineBlobClient.setRichMenuImage(richMenuId,
+      new Blob([bufferA], { type: 'image/png' }));
+    console.log("✅ Uploaded richmenu image");
+
+    // set default
+    await lineclient.setDefaultRichMenu(richMenuId);
+    console.log("✅ Set default richmenu");
+
+    res.json({
+      message: "Richmenu created & set default successfully",
+      richMenuId,
+    });
+  } catch (error) {
+    console.error("❌ Error creating richmenu:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
